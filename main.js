a14e1172b30c24f241bd7cb16c11b87b746f8c35
manahilnadeem <163245288+nadeemtahir@users.noreply.github.com>
2024-05-27 23:29:35 -0700

    Update

---
+++ main.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+let user_ans = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "please enter your sentence"
+});
+let word_count = user_ans.words;
+let clean_para = word_count.split(" ").join("");
+let words_array = word_count.split(" ");
+let filter_words = words_array.filter(words => words !== " ");
+console.log(`you typed ${filter_words.length} words.`);
+console.log(`you typed ${clean_para.length} characters.`);
---
+++ main.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+
+let user_ans = await inquirer.prompt({
+    type:"input",
+    name:"words",
+    message:"please enter your sentence"
+})
+
+let word_count = user_ans.words
+
+let clean_para : string= word_count.split(" ").join("");
+
+let words_array: string[]= word_count.split(" ");
+
+let filter_words= words_array.filter(words => words !== " ");
+
+console.log(`you typed ${filter_words.length} words.`);
+
+console.log(`you typed ${clean_para.length} characters.`);
